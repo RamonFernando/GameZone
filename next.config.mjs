29188a6d7d54ec43c5bcfc13abf504fbaf4c58d2
RamonFernando <ramonrfpa88@gmail.com>
2026-06-08 16:52:27 +0200

    fix: permitir imagenes remotas del catalogo 08062026

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,35 @@ const nextConfig = {
       },
       {
         protocol: "https",
+        hostname: "cdn.akamai.steamstatic.com",
+      },
+      {
+        protocol: "https",
         hostname: "**.g2a.com",
       },
       {
         protocol: "https",
         hostname: "media.rawg.io",
+      },
+      {
+        protocol: "https",
+        hostname: "ddragon.leagueoflegends.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn1.epicgames.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.minecraft.net",
+      },
+      {
+        protocol: "https",
+        hostname: "images.igdb.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.wikia.nocookie.net",
       },
     ],
   },
